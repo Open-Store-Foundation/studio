5b7283d87fe57c91704d3f6db9cac3b5328bb6d0
Andrei Chupin <andrewchupin96@gmail.com>
2025-10-28 13:58:22 +0400

    Fix build

--- src/components/anim/AvoirSkeleton.tsx
+++ src/components/anim/AvoirSkeleton.tsx
@@ -18,10 +18,6 @@ export function AvoirSkeleton(
 
 // Helper function to determine if a component should be displayed
 // when its value is undefined but it's in a loading state
-export const hasValueOrLoading = (value: number | bigint | undefined, isLoading?: boolean): boolean => {
+export const hasValueOrLoading = (value: number | bigint | string | undefined, isLoading?: boolean): boolean => {
     return value !== undefined || isLoading === true;
 };
-
-export const hasValue = (value: any | undefined): boolean => {
-    return value !== undefined && value > 0;
-}
